0940aafd08398528bb25a6ebe8b31dd8ae8c5f33
josvanegmond <jostievanegmond@gmail.com>
2016-07-13 22:05:38 +0200

    Check all, REPLACE instead of INSERT, and fix zero-values overwrite.

--- website/src/administration.php
+++ website/src/administration.php
@@ -25,7 +25,7 @@ if(isset($_POST["upload"])) {
         $studentnumbers = get_student_numbers($csv_data);
         $site = str_replace( "{studentnumbers}", $studentnumbers, $site );
         
-        $studentrows = create_header_weeks($csv_data);
+        $studentrows = create_insert_header_weeks($csv_data);
         $site = str_replace( "{weeks}", $studentrows, $site );
         
         $studentrows = create_insert_student_rows($csv_data);
@@ -37,6 +37,7 @@ if(isset($_POST["upload"])) {
     
     $site = str_replace( "{error}", "Students imported", $site );
     $site = str_replace( "{studentrows}", "", $site );
+    $site = str_replace( "{weeks}", "", $site );
     $site = str_replace( "{importenabled}", "disabled", $site );
     
 } else if(isset($_POST["sqlquery"])) {
@@ -50,13 +51,13 @@ if(isset($_POST["upload"])) {
         $data['studentname'] = $row['nickname'];
         $data['points'] = $row['points']; 
         $data['lastopened'] = $row['lastlogin']; 
-        $studentrows .= create_student_row($data);
+        $studentrows .= create_stats_student_row($data);
     }
     $data['studentnumber'] = "Studentnummer";
     $data['studentname'] = "Studentnaam";
     $data['points'] = "Punten";
     $data['lastopened'] = "Laatst geopend";
-    $headerrow = create_student_row($data);
+    $headerrow = create_stats_student_row($data);
     $site = str_replace( "{studentrows}", $headerrow . $studentrows, $site );
     $site = str_replace( "{whereclause}", $query, $site );
     $site = str_replace( "{error}", "", $site );
@@ -80,28 +81,25 @@ function import_students() {
         $check = $_POST['check-'.$studentnumber];
         if($check == 'on') {
             $pattern = "/week-(\d+)-" . $studentnumber . "/";
+            $weekpoints = Array();
             foreach( $_POST as $key => $value ) {
                 if(preg_match($pattern, $key)) {
                     $points = $value;
                     $weeknumber = preg_replace($pattern, "$1", $key);
-                    echo $weeknumber. ": " . $points . "<br>";
                     $weekpoints[$weeknumber] = $points;
                 }
             }
-            update_student($studentnumber, $weekpoints);
+            update_student_homework($studentnumber, $weekpoints);
         }
     }
 }
 
-function update_student($number, $weekpoints) {
-        //remove existing student from homework table
-        mysql_query( "DELETE FROM homework WHERE student_name = \"".$number."\"" );
+function update_student_homework($number, $weekpoints) {
         
         //insert new
         foreach($weekpoints as $week => $point) {
-            $query = "INSERT INTO homework (weeknumber, grade, student_name)
+            $query = "REPLACE INTO homework (weeknumber, grade, student_name)
                 VALUES(\"" . $week . "\", \"" . $point . "\", \"".$number."\")";
-                echo $query . "<br>";
             mysql_query( $query );
         }
 }
@@ -122,7 +120,8 @@ function get_student_numbers($csv_data) {
     return $studentnumbers;
 }
 
-function create_student_row($row) {
+//create a stats student row
+function create_stats_student_row($row) {
     $studentrow = "<tr>";
     foreach($row as $value) {
         $studentrow .= "<td>" . $value . "</td>";
@@ -131,7 +130,8 @@ function create_student_row($row) {
     return $studentrow;
 }
 
-function create_header_weeks($data) {
+//create the headers for the weeks data on top of the table
+function create_insert_header_weeks($data) {
     $header_weeks = array_keys($data[0]);
     $header_weeks = preg_grep("/week (.*)/i", $header_weeks);
     usort($header_weeks, 'sort_by_week');
@@ -143,28 +143,39 @@ function create_header_weeks($data) {
     return $header;
 }
 
+//sorting helper
 function sort_by_week($week_a, $week_b) {
     $week_a_num = intval(preg_replace("/.*?(\d+).*/", "$1", $week_a));
     $week_b_num = intval(preg_replace("/.*?(\d+).*/", "$1", $week_b));
     return ($week_a_num - $week_b_num);
 }
 
+//create all rows with on each row the student details
 function create_insert_student_rows($data) {
     $studentrows = "";
     
+    //get the table header texts for the weeks
     $header_weeks = array_keys($data[0]);
     $header_weeks = preg_grep("/week (.*)/i", $header_weeks);
+    
+    //sort the weeks in ascending order
     usort($header_weeks, 'sort_by_week');
     
+    //iterate over the csv data
     foreach($data as $row) {
+            
+        //if this participant has a student number, continue. Otherwise, ignore.
         $studentnumber = $row['Gebruikersnaam'];
         if(preg_match("/s\d+/i", $studentnumber)) {
+            
+            //get the current points per week for this student
             $query = "SELECT weeknumber, grade FROM homework WHERE student_name = '" . $studentnumber . "'";
             $result = mysql_query($query);
             while( $currentpoints_row = mysql_fetch_assoc( $result ) ) {
                 $currentpoints[$currentpoints_row["weeknumber"]] = $currentpoints_row["grade"]; 
             }
             
+            //get the new points per week for this student, aligned with the table column name for each week
             foreach($header_weeks as $week_name) {
                 $week_name_short = preg_replace("/(.*?\d+).*/i", "$1", $week_name);
                 $weekpoints[$week_name_short] = preg_replace("/.*?(\d+).*/i", "$1", $row[$week_name]);
@@ -176,6 +187,7 @@ function create_insert_student_rows($data) {
     return $studentrows;
 }
 
+//create a row with student details
 function create_insert_student_row($studentnumber, $currentpoints, $weekpoints) {
     $query = "SELECT * FROM students WHERE name = '" . $studentnumber . "'";
     $studentdata = mysql_fetch_assoc(mysql_query($query));
@@ -183,25 +195,28 @@ function create_insert_student_row($studentnumber, $currentpoints, $weekpoints) 
     $studentclass = $studentdata['class'];
     
     $studentrow = "<tr>";
-    $studentrow .= "<td style='white-space: nowrap;'><input type='checkbox' name='check-".$studentnumber."'></td>";
+    $studentrow .= "<td style='white-space: nowrap;'><input type='checkbox' class='studentcheck' name='check-".$studentnumber."' checked></td>";
     $studentrow .= "<td style='white-space: nowrap;'>" . $studentnumber . "</td>";
     $studentrow .= "<td style='white-space: nowrap;'>" . $studentname . "<input value='".$studentname."' name='name-".$studentnumber."' type='hidden'/></td>";
     $studentrow .= "<td style='text-align: center; white-space: nowrap;'>" . $studentclass . "<input value='".$studentclass."' name='class-".$studentnumber."' type='hidden'/></td>";
     
+    //iterate over each points per week and add a table cell for each
     foreach($weekpoints as $weekname => $points) {
         $weeknumber = preg_replace("/.*?(\d+).*/i", "$1", $weekname);
-        //no change? colour differently
         $currentpoint = $currentpoints[$weeknumber];
+        
+        //assign default values when variables are empty
         if(!isset($currentpoint) || $currentpoint == "" || empty($currentpoint)) {
-            $currentpoint = 0;
+            $currentpoint = -1;
         }
         
         if(!isset($points) || empty($points) || $points == "") {
             $points = 0;
         }
         
-        if(intval($points) == intval($currentpoints[preg_replace("/.*?(\d+).*/i", "$1", $weekname)])) {
-            $studentrow .= "<td style='text-align: center; white-space: nowrap;'><div style='color:#888888;'>" . $points . "</div><input value='" . $points . "' name='week-" . $weeknumber . "-" . $studentnumber . "' type='hidden'/></td>";
+        //no change? colour differently and don´t include in the POST import
+        if(intval($points) == intval($currentpoint)) {
+            $studentrow .= "<td style='text-align: center; white-space: nowrap;'><div style='color:#888888;'>" . $points . "</div></td>";
         } else {
             $studentrow .= "<td style='text-align: center; white-space: nowrap;'>" . $points . "<input value='" . $points . "' name='week-" . $weeknumber . "-" . $studentnumber . "' type='hidden'/></td>";
         }
